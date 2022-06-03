c3ba44a74fd9a1164f65534fd6903b5e5f50c2c7
NyadundoS <nyadundoshirleen@gmail.com>
2022-06-02 17:30:32 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(STDOUT_FILE NO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(STDOUT_FILE NO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
