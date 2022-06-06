886da18efda85c258550266311ebbb2f66d4aa90
NyadundoS <nyadundoshirleen@gmail.com>
2022-06-06 13:40:05 -0700

    if else while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints all possible  different combinations of two digits
+ * Retun: o (when successful)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (y = x+1; y <= '9'; y++)
+		{
+			if (y != x)
+			{
+				putchar(x);
+				putchar(y);
+				if (d == '8' && p == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
