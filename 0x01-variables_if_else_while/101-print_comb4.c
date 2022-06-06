1b32142b13214d5fec10bd4394fe74708ba015ea
NyadundoS <nyadundoshirleen@gmail.com>
2022-06-06 14:05:23 -0700

    if else while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -prints all possible different combinations of three digits
+ * Return: 0 (when successful)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				if ((y != x) != z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == '7' && y == '8' && z == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
