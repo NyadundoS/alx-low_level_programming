08fd4f9adfd93ae159d144e8a7599b6014bd28c8
NyadundoS <nyadundoshirleen@gmail.com>
2022-06-06 11:05:25 -0700

    if else while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers base 10
+ * Return: 0 (when successful)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (1 <= '9')
+	{
+		putchar(i);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
